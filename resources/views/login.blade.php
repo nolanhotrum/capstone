276de74816824663d446d42f1250f8e251c47bfc
Nolan Hotrum <000778258@mohawkcollege.ca>
2023-09-09 00:43:43 -0400

    Made small screen changes to ensure everything works together

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Dog's Way</title>
+
+    </head>
+    <body>
+        <h3>Login</h3>
+    </body>
+</html>
